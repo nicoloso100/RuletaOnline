b49f077fc4594a3e629aefe6c39883765281c857
Nicolas Angarita Ortiz <nico.las0315@hotmail.com>
2020-07-25 22:43:46 -0500

    Configured MongoDB database

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RuletaOnline.Configuration;
 
 namespace RuletaOnline
 {
@@ -24,6 +25,8 @@ namespace RuletaOnline
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var dbConfig = new ServerConfig();
+            Configuration.Bind(dbConfig);
             services.AddControllers();
         }
 
